e67264fbacc471cb0f66d70f1232d45feacf71d6
Ian <iubaldo2@illinois.edu>
2021-04-02 18:03:51 -0500

    implemented rudimentary heat system

--- Scripts/Voxel.gd
+++ Scripts/Voxel.gd
@@ -7,24 +7,27 @@ onready var heatGradient = preload("res://Shaders/Gradients/HeatGradient2.tres")
 
 # metal attributes
 var voxelStrikePenalty = 0 # determines how much more power is needed to strike this voxel
-var heatResist = 5 # determines heat increase rate, range(0 - 10)
+var heatResist = 0 # determines heat increase/decrease rate modifier, range(0 - 100)
 var heatTol = 2000 # determines max heat before voxel melts and is removed from the grid
 var minForgeTemp = 1000 # min heat required to forge, applies strike power penalty
 var optForgeTemp = 1200 # optimal heat to forge, no strike penalty
 
 var heat = 0.0 # range(0, heatTol)
+var heatLossRate = -10 # determines rate of heat decrease
 
 var isTargeted = false
 
 func _physics_process(delta):
 	outlineMesh.visible = true if isTargeted else false
+	
+	setHeat(heatLossRate, delta)
 		
 	var mat = SpatialMaterial.new()
 	mat.albedo_color = getMaterialColor()
 	mesh.set_surface_material(0, mat)
 	
-	heat = clamp(heat + 5, 0, heatTol)
-	print(var2str(heat / heatTol))
+func setHeat(rate, delta):
+	heat = clamp(heat + (rate * ((100 - heatResist) / 100)) * delta, 0, heatTol)
 		
 func getMaterialColor():
 	var scaledHeat = heat / heatTol
--- Scripts/VoxelHandler.gd
+++ Scripts/VoxelHandler.gd
@@ -4,6 +4,7 @@ onready var voxel = preload("res://Scenes/Voxel.tscn")
 onready var voxelGrid = get_node("VoxelGrid")
 onready var powerLabel = get_node("SmithingUI/PowerLabel")
 onready var remainingVoxelsLabel = get_node("SmithingUI/RemainingVoxelsLabel")
+onready var heatLabel = get_node("SmithingUI/HeatLabel")
 onready var strikeTimer = get_node("StrikeTimer")
 
 var voxelList = []
@@ -16,6 +17,10 @@ var remainingVoxels = 50
 var maxPower = 500
 var strikePower = 0
 var powerIncreaseRate = 250
+
+var lightHeatLoss = 25
+var medHeatLoss = 50
+var heavyHeatLoss = 100
 
 var cameraForward = Vector3.FORWARD
 var cameraBack =  Vector3.BACK
@@ -31,6 +36,8 @@ func _process(delta):
 		powerLabel.text = ""
 		
 	remainingVoxelsLabel.text = "Remaining Metal: " + var2str(remainingVoxels)
+	heatLabel.text = "Heat: " + var2str(int(voxelGrid.heat))
+		
 		
 	voxelList = voxelGrid.get_children()
 		
@@ -54,6 +61,10 @@ func _process(delta):
 	if Input.is_action_just_released("pointer"):
 		strikeTimer.start()
 		
+	if Input.is_action_pressed("shift"):
+		for vox in voxelList:
+			vox.setHeat(200, delta)
+		
 	# add ingot (debug)
 	if Input.is_action_just_pressed("1"):
 		addIngot(1)
@@ -76,7 +87,7 @@ func _process(delta):
 func _physics_process(delta):
 	if !voxelList.empty():
 		for vox in voxelList:
-			if vox.isTargeted:
+			if vox != null && vox.isTargeted:
 				targetVoxel = vox
 				break
 		
@@ -108,12 +119,16 @@ func Strike(target, power):
 			var vox = voxel.instance()
 			voxelGrid.add_child(vox)
 			vox.translation = pos
+			vox.heat = voxelGrid.heat
 			remainingVoxels -= 1
+					
+	targetVoxel.heat = clamp(targetVoxel.heat - 50, 0, targetVoxel.heatTol)
 		
 func checkExistingVoxel(targetPos):
-	for pos in voxelList:
-		if pos.translation == targetPos:
-			print("voxel found at position: " + var2str(pos.translation))
+	for vox in voxelList:
+		if vox.translation == targetPos:
+			print("occupied: " + var2str(vox.translation))
+			vox.heat = clamp(targetVoxel.heat - 25, 0, targetVoxel.heatTol)
 			return null
 	
 	print("position added")
@@ -128,6 +143,8 @@ func addIngot(size):
 						var vox = voxel.instance()
 						voxelGrid.add_child(vox)
 						vox.translation = Vector3(x, 0, z)
+						
+		voxelGrid.translation = Vector3(0.5, 0.5, -0.5)
 	else:
 		match size:
 			1:
